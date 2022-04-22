4f01f04f0a1cf961fdd76aa32878b312d7145924
Kou Matsumoto <develop@kou-matsumoto.jp>
2022-04-22 18:51:59 +0900

    feat: filter caaj by timerange

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ import sys
 
 if __name__ == '__main__':
   args = sys.argv
-  chain = args[1]
-  address = args[2]  
+  chain = 'osmosis'
+  address = 'osmo1xcqshxpmhdc4tfcscxpmdlt9kwql6qm5wc9ger'
   setting = {}
   if chain == 'bsc':
     setting['bscscan_key'] = os.environ['BSCSCAN_KEY']
-  senka = Senka(setting, './pyproject.toml')
-  caaj = senka.get_caaj_csv(chain, address, )
+  senka = Senka(setting, '../pyproject.toml')
+  # caaj = senka.get_caaj_csv(chain, address)
+  caaj = senka.get_caaj_csv(chain, address, starttime=1650608478, endtime=1650608827)
   print(caaj)
-  
