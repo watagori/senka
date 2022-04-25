f353416a7fc489c5448ed28e217001114ab957dc
Kou Matsumoto <develop@kou-matsumoto.jp>
2022-04-25 16:12:29 +0900

    revert main.py

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ import sys
 
 if __name__ == '__main__':
   args = sys.argv
-  chain = 'osmosis'
-  address = 'osmo1xcqshxpmhdc4tfcscxpmdlt9kwql6qm5wc9ger'
+  chain = args[1]
+  address = args[2]  
   setting = {}
   if chain == 'bsc':
     setting['bscscan_key'] = os.environ['BSCSCAN_KEY']
-  senka = Senka(setting, '../pyproject.toml')
-  # caaj = senka.get_caaj_csv(chain, address)
-  caaj = senka.get_caaj_csv(chain, address, starttime=1650608478, endtime=1650608827)
+  senka = Senka(setting, './pyproject.toml')
+  caaj = senka.get_caaj_csv(chain, address, )
   print(caaj)
+  
